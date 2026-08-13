689d26951dae63ee9e7c5c188c36fd6a091d1bc3
socriatesaket <kumarsaket3453@gmail.com>
2026-08-13 18:51:53 +0000

    variables knowledge

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId=8809
+let accountEmail="saket8292@gp.com"
+var accPassword="3654"
+acccity="jaipur"
+let accountstate;
+
+//will give error as we cant changeconst accountId=2
+console.log("accountId")
+accPassword="1268"
+accountEmail="sakshi@gp.com"
+console.table([accountId,accountEmail,accPassword,acccity,accountstate])
+/*
+prefer not to use var due to issue in vlock scpe and  functional scope
+*/ 
